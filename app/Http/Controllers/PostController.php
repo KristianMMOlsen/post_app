9b6c36c03b0574eb90a2e269e6f5023c7f261dd1
Kristian Olsen <33516139+KristianMMOlsen@users.noreply.github.com>
2021-03-11 16:35:06 +0100

    Added a factory to create test data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(10); //returns a list of 10 posts per page
+        $posts = Post::paginate(20); //returns a list of 20 posts per page
 
         return view('posts.index', [
             'posts' => $posts
